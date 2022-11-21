7e22937a31adcf115516e1e537f12457bc06d026
Sid Lacy <contact@swlacy.com>
2022-11-20 21:23:15 -0800

    structure

--- subreddit-stats.py
+++ subreddit-stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from requests import get
-import json
+from json import loads
 from datetime import datetime
 
 BASE_URL = 'https://reddit.com/subreddits/new.json?limit=100'
@@ -10,46 +10,51 @@ HEADERS = {
 }
 TIMEOUT = 10
 MAX_DESC_LEN = 120
-OUT_CSV = f'./{datetime.today().strftime("%Y-%m-%d.%H:%M:%S")}.out.csv'
+OUT_FILE = f'./{datetime.today().strftime("%Y-%m-%d.%H:%M:%S")}.out.csv'
 
 def main():
-    # Write CSV headers
-    with open(OUT_CSV, 'w', encoding = 'utf-8') as file:
-        file.write('sub_name,sub_subs,sub_date,sub_type,sub_quar,sub_nsfw,sub_desc\n')
+    # Open the output file (OUT_FILE); write CSV headers
+    with open(OUT_FILE, 'w', encoding = 'utf-8') as file:
+        file.write('Updated,Subreddit,Created,Subscribers,NSFW,Quarantined,Description\n')
     # Set the url to request, sans next ID on first run
     url = f'{BASE_URL}'
     # Record count tracker
     count = 0
+    # This process takes a long time
     while True:
-        #Fetch the first page of results
-        response = json.loads(get(url, headers=HEADERS, timeout=TIMEOUT).text)
-        for sub_id in response['data']['children']:
+        #Fetch data for the first page of results
+        response = loads(get(url, headers=HEADERS, timeout=TIMEOUT).text)
+        for row in response['data']['children']:
+            # Record update time
+            updated = datetime.today().isoformat()
             # Fetch each element
-            sub_name = sub_id['data']['display_name']
-            sub_subs = sub_id['data']['subscribers']
-            sub_date = datetime.fromtimestamp(sub_id['data']['created']).date()
-            sub_type = sub_id['data']['subreddit_type']
-            sub_quar = sub_id['data']['quarantine']
-            sub_nsfw = sub_id['data']['over18']
-            sub_desc = sub_id['data']['public_description'].replace(",", "").replace("\n", "")
+            subreddit = row['data']['display_name']
+            created = row['data']['created']
+            subscribers = row['data']['subscribers']
+            nsfw = row['data']['over18']
+            quarantined = row['data']['quarantine']
+            description = row['data']['public_description']
+            # Format each element as needed
+            created = datetime.fromtimestamp(created).isoformat()
+            description = description.replace(",", "").replace("\n", "")
             # Truncate description lenth
-            if len(sub_desc) > MAX_DESC_LEN - 1:
-                sub_desc = sub_desc[0:MAX_DESC_LEN - 1] + '...'
-            if not sub_desc:
-                sub_desc = '<No description>'
+            if len(description) > MAX_DESC_LEN:
+                description = description[0:MAX_DESC_LEN - 1]
+            if not description:
+                description = '!NO_DESCRIPTION'
             # Form final record
-            record = f'{sub_name},{sub_subs},{sub_date},{sub_type},{sub_quar},{sub_nsfw},{sub_desc}\n'
+            record = f'{updated},{subreddit},{created},{subscribers},{nsfw},{quarantined},{description}\n'
             # Skip record if it already exists
-            with open(OUT_CSV, 'r', encoding = 'utf-8') as file:
-                if f'\n{sub_name}' in file.read():
+            with open(OUT_FILE, 'r', encoding = 'utf-8') as file:
+                if f'{subreddit},{created}' in file.read():
                     # STDOUT log
-                    print(f'[+{count}] ({datetime.now()}): skipping duplicate record for \'r/{sub_name}\'')
+                    print(f'[?] (#{count}@{updated}): skipping duplicate record for \'r/{subreddit}\'')
                     continue
-            # Otherwise, record the result in OUT_CSV; increment counter
-            with open(OUT_CSV, 'a', encoding = 'utf-8') as file:
+            # Otherwise, record the result in OUT_FILE; increment counter
+            with open(OUT_FILE, 'a', encoding = 'utf-8') as file:
                 file.write(record)
                 # STDOUT log; increment counter
-                print(f'[+{count}] ({datetime.now()}): successfully recorded \'/r/{sub_name}\' (https://reddit.com/r/{sub_name})')
+                print(f'[+] (#{count}@{updated}): added record for \'r/{subreddit}\'')
                 count += 1
         # Append the next page ID to the url for the next request
         url = f'{BASE_URL}&after={response["data"]["after"]}'
